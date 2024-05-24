165771c2d28d4d579d46d82abbd87e2d3567acab
jonas felipe quirino <jonas-felipe16@hotmail.com>
2024-05-24 20:33:41 -0300

    create splash and onBoarding view

---
+++ lib/app/modules/core/_export_core.dart
@@ -0,0 +1,3 @@
+export 'components/button_component.dart';
+export 'constants/app_color_constant.dart';
+export 'constants/app_text_style_constant.dart';
---
+++ lib/app/modules/splash/pages/on_boarding_page.dart
@@ -0,0 +1,66 @@
+import 'dart:developer';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '../../core/_export_core.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({super.key});
+
+  Widget _text({
+    required String title,
+    TextStyle? textStyle,
+  }) =>
+      Text(
+        title,
+        style: textStyle,
+      );
+
+  Widget _padding({required double top}) =>
+      Padding(padding: EdgeInsets.only(top: top));
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: AppColorConstant.backgroundOnBoarding,
+                child: Image.asset("assets/images/on_boarding.png"),
+              ),
+            ),
+            _padding(top: 16.0),
+            _text(
+                title: "Spend Smarter",
+                textStyle: AppTextStyleConstant.onBoardingTextStyle),
+            _text(
+                title: "Save More",
+                textStyle: AppTextStyleConstant.onBoardingTextStyle),
+            const ButtonComponent(text: "Get Started"),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Already have account? ",
+                    style: AppTextStyleConstant.smallText.copyWith(
+                      color: AppColorConstant.greyColor,
+                    ),
+                  ),
+                  TextSpan(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () => log('message'),
+                    text: "Sign in",
+                    style: AppTextStyleConstant.smallText.copyWith(),
+                  ),
+                ],
+              ),
+            ),
+            _padding(top: 40.0)
+          ],
+        ),
+      ),
+    );
+  }
+}
